57881e6a2ace86bb00f49a559c5ff8387a79cb1a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-19 12:49:24 -0500

    example5 working GPSTk 2.10

--- example5.py
+++ example5.py
@@ -9,51 +9,41 @@ Usage:
   python sem_plot.py
 
 """
-
-
 import gpstk
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure,show
 
 
-def main():
-    # Read in data, strict=True makes dataSets a list rather than a generator:
-    header, dataSets = gpstk.readSEM('data/sem_data.txt', strict=True)
+# Read in data, strict=True makes dataSets a list rather than a generator:
+header, dataSets = gpstk.readSEM('data/sem_data.txt', strict=True)
 
-    # Write the data back to a different file (their contents should match):
-    gpstk.writeSEM('sem_data.txt.new', header, dataSets)
+# Write the data back to a different file (their contents should match):
+gpstk.writeSEM('sem_data.txt.new', header, dataSets)
 
-    # Read the orbit out of the data:
-    orbit = dataSets[0].toAlmOrbit()  # alm orbit of first data point
+# Read the orbit out of the data:
+orbit = dataSets[0].toAlmOrbit()  # alm orbit of first data point
 
-    austin = gpstk.Position(30, 97, 0, gpstk.Position.Geodetic)  # Austin, TX
+austin = gpstk.Position(30, 97, 0, gpstk.Position.Geodetic)  # Austin, TX
 
-    starttime = gpstk.CommonTime()    # iterator time to start at
-    starttime.setTimeSystem(gpstk.TimeSystem('GPS'))
-    endtime = gpstk.CommonTime()  # end time, 1 day later (see below)
-    endtime.setTimeSystem(gpstk.TimeSystem('GPS'))
-    endtime.addDays(1)
-
-    X = []
-    Y = []
-
-    # Step through a day, adding plot points:
-    for t in gpstk.times(starttime, endtime, seconds=1000):
-        xvt = orbit.svXvt(t)
-        location = gpstk.Position(xvt.x)
-        elevation = austin.elevation(location)
-        X.append(t.getDays())
-        Y.append(elevation)
-
-    # Make the plot
-    fig = plt.figure()
-    fig.suptitle('Elevation of a GPS satellite throughout the day',
-                 fontsize=14, fontweight='bold')
-    ax = fig.add_subplot(111)
-    ax.set_xlabel('Time (days)')
-    ax.set_ylabel('Elevation (degrees)')
-    plt.plot(X, Y, 'r')
-    plt.show()
+starttime = gpstk.CommonTime()    # iterator time to start at
+starttime.setTimeSystem(gpstk.TimeSystem('GPS'))
+endtime = gpstk.CommonTime()  # end time, 1 day later (see below)
+endtime.setTimeSystem(gpstk.TimeSystem('GPS'))
+endtime.addDays(1)
+#%% Step through a day, adding plot points:
+d = []
+el = []
+for t in gpstk.times(starttime, endtime, seconds=1000):
+    d.append(t.getDays())
+    xvt = orbit.svXvt(t)
+    location = gpstk.Position(xvt.x)
+    el.append(austin.elevation(location))
 
 
-if __name__ == '__main__':
-    main()
+# Make the plot
+fig = figure()
+ax = fig.gca()
+ax.plot(el)
+ax.set_xlabel('Time (days)')
+ax.set_ylabel('Elevation (degrees)')
+ax.set_title('Elevation of a GPS satellite throughout the day')
+show()
