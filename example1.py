a785a9dd806e397054ea0211bec04e690eb456b9
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-19 12:11:33 -0500

    example 1 working GPSTk 2.10

--- example1.py
+++ example1.py
@@ -14,44 +14,40 @@ Usage:
 import gpstk
 
 
-def main():
-    # In the GPSTk there are multiple classes to manage time, depending
-    # on the specific operation that we want to carry out. This modular
-    # approach eases handling the many different time systems used in the
-    # modern Global Navigation Satellite Systems.
 
-    # Note, however, that in the GPSTk the unifying class to do time
-    # Computations is the 'CommonTime' class.
+# In the GPSTk there are multiple classes to manage time, depending
+# on the specific operation that we want to carry out. This modular
+# approach eases handling the many different time systems used in the
+# modern Global Navigation Satellite Systems.
 
-    # Read current time from system clock
-    systime = gpstk.SystemTime()
+# Note, however, that in the GPSTk the unifying class to do time
+# Computations is the 'CommonTime' class.
 
-    # Convert to 'CommonTime', the standard way to handle time at GPSTk
-    comtime = systime.toCommonTime()
+# Read current time from system clock
+systime = gpstk.SystemTime()
 
-    # This is the typical way to handle civil time
-    civtime = gpstk.CivilTime(comtime)
+# Convert to 'CommonTime', the standard way to handle time at GPSTk
+comtime = systime.toCommonTime()
 
-    # The YDSTime class is very useful for common GNSS tasks
-    ydstime = gpstk.YDSTime(comtime)
+# This is the typical way to handle civil time
+civtime = gpstk.CivilTime(comtime)
 
-    # This is a typical class to handle time in GPS system
-    gpstime = gpstk.GPSWeekSecond(comtime)
+# The YDSTime class is very useful for common GNSS tasks
+ydstime = gpstk.YDSTime(comtime)
 
-    # Class to handle Modified Julian Date
-    mjd = gpstk.MJD(comtime)
+# This is a typical class to handle time in GPS system
+gpstime = gpstk.GPSWeekSecond(comtime)
 
-    print("Hello world!")
-    print("   The current civil time is", civtime)
-    print("   The current year is", ydstime.year)
-    print("   The current day of year is", ydstime.doy)
-    print("   The current second of day is", ydstime.sod)
-    print("   The current full GPS week is", gpstime.week)
-    print("   The current short GPS week is", gpstime.getModWeek())
-    print("   The current day of GPS week is", gpstime.getDayOfWeek())
-    print("   The current second of GPS week is", gpstime.sow)
-    print("   The current Modified Julian Date is", mjd)
+# Class to handle Modified Julian Date
+mjd = gpstk.MJD(comtime)
 
-
-if __name__ == '__main__':
-    main()
+print("Hello world!")
+print("   The current civil time is {}-{}-{}T{}:{}:{}".format(civtime.year,civtime.month,civtime.day,civtime.hour,civtime.minute,civtime.second))
+print("   The current year is", ydstime.year)
+print("   The current day of year is", ydstime.doy)
+print("   The current second of day is", ydstime.sod)
+print("   The current full GPS week is", gpstime.week)
+print("   The current short GPS week is", gpstime.getModWeek())
+print("   The current day of GPS week is", gpstime.getDayOfWeek())
+print("   The current second of GPS week is", gpstime.sow)
+print("   The current Modified Julian Date is", mjd.mjd)
